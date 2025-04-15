e6fbde11f0bdf0a0105e598518f74aa373795f28
Ma. Kristine R. Mien <mcmien@gbox.adnu.edu.ph>
2025-04-16 03:05:20 +0800

    added design

--- main.dart
+++ main.dart
@@ -9,7 +9,7 @@
     Users can add, edit, or delete partitions anytime.
    */
   
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
